55afd7b621e4415e9d12cf4ad3fc9180b90c5c5d
ksandrsl <ksandr.sl@gmail.com>
2023-05-08 23:15:24 +0300

    feat(04-copy-directory): complete task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const { mkdir, readdir, copyFile, access } = require('fs/promises');
+const folder = path.join(__dirname, 'files');
 
+function copyDir(files) {
+  const copyFolder = path.join(__dirname, 'files-copy');
+
+  fs.rm(copyFolder, { recursive: true }, error => {
+    if (error) {
+      console.log('create folder');
+      mkdir(copyFolder, { recursive: true });
+    } else {
+      fs.rm(copyFolder, { recursive: true }, error => {
+        console.log('delete and create folder');
+        mkdir(copyFolder, { recursive: true });
+      })
+    }
+
+    readdir(files).then(files => {
+      for (file of files) {
+        copyFile(path.join(__dirname, 'files', file), path.join(copyFolder, file));
+      }
+    })
+
+  })
+
+}
+
+copyDir(folder);
